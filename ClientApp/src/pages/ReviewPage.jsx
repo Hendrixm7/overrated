a48926abeebd78e82bbc116f13c8730b08f24e55
Michelle Hendrix <michelle49@mail.usf.edu>
2020-04-23 22:21:07 -0400

    Build Out Review Page

--- ClientApp/src/pages/ReviewPage.jsx
+++ ClientApp/src/pages/ReviewPage.jsx
@@ -1,11 +1,64 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import axios from 'axios'
+import { useHistory } from 'react-router-dom'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
-export function ReviewPage() {
+import { useQuery } from '../hooks'
+
+export function ReviewPage(props) {
+  const history = useHistory()
+  const query = useQuery()
+  const [restaurantResult, setRestaurantResult] = useState({})
+  const [overrated, setOverrated] = useState(null)
+  const [comment, setComment] = useState('')
+
+  const handleSubmit = () => {
+    axios
+      .post('api/Feedback', {
+        restaurantId: parseInt(query.get('id')),
+        overrated,
+        comment,
+      })
+      .then(() => {
+        history.push(`/restaurant/${query.get('id')}`)
+      })
+  }
+
+  const handleTextChange = e => {
+    setComment(e.target.value)
+  }
+
+  useEffect(() => {
+    const id = query.get('id')
+    axios
+      .get(`api/Restaurant/${id}`)
+      .then(response => setRestaurantResult(response.data))
+  }, [props.match.params.id])
+
   return (
     <div className="review-page">
-      <h1 className="restaurant-name">Restaurant Name</h1>
-      <textarea placeholder="type your review here" />
-      <button>Submit Review</button>
+      <h1 className="restaurant-name">{restaurantResult.name}</h1>
+      <div className="rating-icons">
+        <FontAwesomeIcon
+          icon={['far', 'grin']}
+          className={overrated !== null && !overrated ? 'active' : ''}
+          onClick={() => setOverrated(false)}
+        />
+        <FontAwesomeIcon
+          icon={['far', 'meh']}
+          className={overrated !== null && overrated ? 'active' : ''}
+          onClick={() => setOverrated(true)}
+        />
+      </div>
+      <textarea
+        placeholder="type your review here"
+        rows={15}
+        value={comment}
+        onChange={handleTextChange}
+      />
+      <button className="review-button" onClick={handleSubmit}>
+        Submit Review
+      </button>
     </div>
   )
 }
